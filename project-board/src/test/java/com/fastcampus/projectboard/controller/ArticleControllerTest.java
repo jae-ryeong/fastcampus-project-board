a4b0566820d4ae2d8a64212f644420dba538021a
김재령 <wofud0321@naver.com>
2023-01-10 11:25:00 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어주고
    뷰 적절히 뼈대 잡음
    테스트에서는 media type검사에 부가적인 옵션이
    들어갈 수 있음을 확인했기 때문에
    적절히 테스트 메소드를 수정

--- project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -24,7 +24,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -33,7 +32,7 @@ class ArticleControllerTest {
         // when & then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())    // 검사식
-                .andExpect(content().contentType(MediaType.TEXT_HTML))  // view를 검사하므로 TEXT_HTML 타입인지 검사한다
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))  // view를 검사하므로 TEXT_HTML 타입인지 검사한다, 호환되는 타입까지도 검사해서 허용해준다(더 넓은 범위의 허용)
                 .andExpect(view().name("articles/index"))   // 어떤 뷰 네임이 있어야한다.
                 .andExpect(model().attributeExists("articles")); // attribute가 있나 없나를 검사한다.
 
